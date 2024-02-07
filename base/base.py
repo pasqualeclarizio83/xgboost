0d7149e0d4432d0364909bf3a4b16ce03bff3d64
pasqualeclarizio83 <pascalclarizio@gmail.com>
2024-02-07 14:24:36 +0100

    base

---
+++ base/base.py
@@ -0,0 +1,5 @@
+import xgboost as xgb
+from xgboost import XGBClassifier
+from xgboost import XGBRegressor
+model = XGBClassifier()  # Ad esempio, un classificatore XGBoost
+print(model)  # Stampa le informazioni del modello
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Nome del Tuo Progetto
+
+XgBoost
+
+## Descrizione
+
+Le basi di XgBoost
+
+## Requisiti del Sistema
+
+xgboost==1.6.2
+
+## Installazione
+
+```bash
+pip install xgboost==1.6.2
+```
